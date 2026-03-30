059498e818a05253e1547463de182bff20bc5647
JonathasNJohnny <jonathasnobrega59@gmail.com>
2026-03-30 00:31:45 -0300

    1 - close to tray;

--- src/electron/main.js
+++ src/electron/main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow, ipcMain } = require("electron");
+const { app, BrowserWindow, Menu, Tray, ipcMain } = require("electron");
 const path = require("path");
 const fs = require("fs");
 const {
@@ -11,6 +11,67 @@ const { createSetupWindow } = require("./windows/setup-window");
 
 // importa seu servidor (será recarregado após configurar)
 let serverModule = null;
+let mainWindow = null;
+let tray = null;
+let isQuitting = false;
+
+function showMainWindow() {
+  if (!mainWindow || mainWindow.isDestroyed()) {
+    return;
+  }
+
+  if (!mainWindow.isVisible()) {
+    mainWindow.show();
+  }
+
+  if (mainWindow.isMinimized()) {
+    mainWindow.restore();
+  }
+
+  mainWindow.focus();
+}
+
+function createTray() {
+  if (tray) {
+    return;
+  }
+
+  const iconPath = path.join(__dirname, "..", "..", "icon.png");
+  tray = new Tray(iconPath);
+  tray.setToolTip("SucatasBot");
+
+  tray.on("double-click", () => {
+    showMainWindow();
+  });
+
+  tray.on("click", () => {
+    showMainWindow();
+  });
+
+  const trayMenu = Menu.buildFromTemplate([
+    {
+      label: "Abrir",
+      click: () => {
+        showMainWindow();
+      },
+    },
+    // {
+    //   label: "Lançar Teste de Carta",
+    //   click: () => {
+    //     showMainWindow();
+    //   },
+    // },
+    {
+      label: "Sair",
+      click: () => {
+        isQuitting = true;
+        app.quit();
+      },
+    },
+  ]);
+
+  tray.setContextMenu(trayMenu);
+}
 
 function loadServer() {
   if (serverModule) {
@@ -36,14 +97,25 @@ contextBridge.exposeInMainWorld('electron', {
 
 function createWindowsAfterSetup() {
   // janela de controle
-  const twitchControl = new BrowserWindow({
+  mainWindow = new BrowserWindow({
     width: 900,
     height: 700,
     icon: path.join(__dirname, "..", "..", "icon.png"),
   });
 
-  twitchControl.setTitle("Lizah Gacha");
-  twitchControl.loadURL("http://localhost:49382/twitchControl.html");
+  mainWindow.setTitle("Lizah Gacha");
+  mainWindow.loadURL("http://localhost:49382/twitchControl.html");
+
+  mainWindow.on("close", (event) => {
+    if (isQuitting) {
+      return;
+    }
+
+    event.preventDefault();
+    mainWindow.hide();
+  });
+
+  createTray();
 }
 
 app.whenReady().then(() => {
@@ -93,4 +165,12 @@ app.whenReady().then(() => {
     loadServer();
     setTimeout(createWindowsAfterSetup, 1000);
   }
+});
+
+app.on("before-quit", () => {
+  isQuitting = true;
+});
+
+app.on("activate", () => {
+  showMainWindow();
 });
